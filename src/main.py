c35a039ed318d479da33e600ef3467dbef103d5f
Gabriel Choong <75243053+gabrielchoonggeliang@users.noreply.github.com>
2022-11-28 17:36:00 +0800

    Created main.py

---
+++ src/main.py
@@ -0,0 +1,14 @@
+# Connecting nodes
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+
+
+
+
+def main():
+    if __name__ == '__main__':
+        main()
