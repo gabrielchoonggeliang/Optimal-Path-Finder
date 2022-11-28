a56adedca4a180d0de29619782074d21daac96e5
Gabriel Choong <75243053+gabrielchoonggeliang@users.noreply.github.com>
2022-11-28 17:02:55 +0800

    Updated files and created functions.py

---
+++ src/functions.py
@@ -0,0 +1,10 @@
+# class for user input
+class user_input:
+    def __init__(self, name):
+        self.name = name
+    
+    def get_name(self):
+        return self.name
+    
+    def set_name(self, name):
+        self.name = name
